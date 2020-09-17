0423cdd6b99573bdf7f61271abf8358cf5cc10b3
mocotan <19368710+mocotanus@users.noreply.github.com>
2020-09-17 10:40:31 +0900

    add: 制御構文 try if文の場合を追加

---
+++ Seq2/controlSyntax.go
@@ -0,0 +1,31 @@
+package main
+
+import "strconv"
+
+func main() {
+
+	println("(･ω･)if文を使った場合~~~~~~~~~~")
+	for i := 1; i <= 100; i = i + 1 {
+		str := ""
+		if i%2 == 0 {
+			str = "偶数"
+		} else {
+			str = "奇数"
+		}
+
+		// println(string(i) + "-")// A,Bになってしまった
+		// 整数値は Unicode コードポイントとして解釈されるとのこと
+		//https://www.delftstack.com/ja/howto/go/how-to-convert-an-int-value-to-string-in-go/
+
+		// 文字列変換関係は「strconv」パッケージのItoa 関数を使えばいい
+		println(strconv.Itoa(i) + "-" + str)
+	}
+
+	println("")
+	println("(･ω･)switchを使った場合~~~~~~~~~~")
+	for i := 1; i <= 100; i = i + 1 {
+		str := ""
+
+		println(strconv.Itoa(i) + "-" + str)
+	}
+}
