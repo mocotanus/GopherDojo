d617e5be659999943b14f340db2b6cb7808b406c
mocotan <19368710+mocotanus@users.noreply.github.com>
2020-09-18 17:03:43 +0900

    add: 組み込み型(数値)

--- Seq3/type.go
+++ Seq3/type.go
@@ -1,4 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
+	// 【TRY】組み込み型（数値）
+	var sum int
+	sum = 5 + 6 + 3
+	avg := sum / 3 // avgには4が入っている
+	// avg := float64(sum) / 3.0
+	println(avg)
+
+	// コンパイルエラー：constant 4.5 truncated to integer
+	//4.5が整数じゃないため
+
+	// 4.5の型を調べる
+	fmt.Printf("%T\n", 4.5) // 4.5の型はfloat64
+
+	// if avg > 4.5 {
+	if float64(avg) > 4.5 {
+		println("good")
+	}
+
+	// 型の違う値は計算できないらしい
+
 }
